99a76cff23eea424e9f640794abe24ce9595684c
anaghar <ranagha40@gmail.com>
2021-10-24 20:30:24 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './common/header/Header';
+
 import './index.css';
+import Home from './screens/home/Home';
 
 
 
-ReactDOM.render(<Header/>,document.getElementById('root')
+ReactDOM.render(<Home/>,document.getElementById('root')
 );
 
 
